bf94d6c41f51c27f91a65a388874290c68fcf99f
FaveAsian <38636488+FaveAsian@users.noreply.github.com>
2022-09-21 20:02:01 -0500

    Updated code for twitter api V2

--- yordle_scout.py
+++ yordle_scout.py
@@ -18,7 +18,7 @@ twitter_client = tweepy.Client(bearer_token=os.getenv("BEARER_TOKEN"))
 async def on_ready():
     stream = AsyncStreamingClient(bearer_token=os.getenv("BEARER_TOKEN"))
     # stream rule add to follow LoLDev
-    await stream.add_rules(tweepy.StreamRule("from:LoLDev"))
+    await stream.add_rules(tweepy.StreamRule("from:LoLDev OR from:LeagueOfLegends"))
     stream.filter()
     print('Logged on as {0}!'.format(client.user))
     await client.change_presence(
@@ -28,19 +28,17 @@ async def on_ready():
 
 class AsyncStreamingClient(tweepy.asynchronous.AsyncStreamingClient):
     async def on_tweet(self, tweet):
-        print(tweet.data)
-        # Dynamically grab username of tweets
-        #author_username = twitter_client.get_tweet(id=tweet.id, expansions="author_id", 
-        #                                            user_fields="username", tweet_fields="entities").includes["users"][-1]
-        tweet_include = twitter_client.get_tweet(id=tweet.id, expansions=[ "attachments.media_keys"], media_fields=["url"]).includes
-        
-        link = "https://www.twitter.com/LoLDev/status/" + str(tweet.id)
+        tweet_include = twitter_client.get_tweet(id=tweet.id, expansions=["author_id", "attachments.media_keys"], media_fields=["url"]).includes
+
+        link = "https://www.twitter.com/" + str(tweet_include["users"][-1]) + "/status/" + str(tweet.id)
         channel = client.get_channel(int(os.getenv("GENERAL_CHANNEL")))
         await channel.send(link)
         if "media" in tweet_include:
             for media in tweet_include["media"]:
                 if media.type == "photo":
                     await channel.send(media.url)
+                if media.type == "video":
+                    await channel.send("Tweet has video attachment")
 
     async def on_connect(self):
         print("Stream has connected")
